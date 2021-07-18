ca7ea2813597eb3500271ac8b03c2d2094863754
João Pedro Ferraz <joaoferrazp@gmail.com>
2021-07-18 19:00:17 -0300

    implementa busca com filtro e tratamento de datas

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -9,9 +9,21 @@ use App\Models\Event;
 class EventController extends Controller
 {
     public function index(){  
-        $events = Event::all();     
+
+        $search = request('search');
+
+        if($search){
+
+            $events = Event::where([
+                ['title', 'like','%'.$search.'%']
+            ])->get();
+
+        } else {
+             $events = Event::all();     
+        }
+       
     
-    return view('welcome',['events'=>$events]);
+    return view('welcome',['events'=>$events,'search'=>$search]);
 
     }
 
@@ -23,11 +35,12 @@ class EventController extends Controller
 
         $event = new Event;
         $event->title = $request->title;
+        $event->date= $request->date;
         $event->city=$request->city;
         $event->private=$request->private;
         $event->description=$request->description;
         $event->items=$request->items;
-        
+       
 
         //Image upload
 
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,28 +5,37 @@
 
 @section('content')
 
+
 <div id="search-container" class="col-md-12">
     <h1> Busque um evento <h1>
-        <form action="">
+        <form action="/" method="GET">
             <input type="text" id="search" name="search" class="form-control" placeholder="Procurar evento">
         </form>
 </div>        
         <div id="events-container" class="col-md-12">
-            <h2>Próximos Eventos</h2>
+           @if ($search)
+            <h2>Buscando por : {{$search}} </h2>
+            @else
+            <h2>Proximos Eventos</h2>
+            @endif
             <p class="subtitle">Veja os eventos dos próximos dias</p>
             <div id="cards-container" class="row">
                 @foreach ($events as $events )
                 <div class="card col-md-3">
                     <img src="/img/events/{{ $events->image }}" alt="{{$events->tile}}">
                     <div class="card-body">
-                        <p class="card-date"> 10/09/2020 </p>
+                        <p class="card-date"> {{date('d/m/Y'), strtotime($events->date)}} </p>
                         <h5 class="card-title"> {{ $events->title}} </h5>
                         <p class="card-participants"> X Participantes </p>                        
                         <a href="/events/{{$events->id}}" class="btn btn-secondary">Saber mais</a>
                     </div>   
                 </div>    
-                @endforeach    
-                
+                @endforeach  
+                @if($search )
+                    <p>Não foi possivel encontrar nenhum evento com {{$search}} <a href="/"> Ver todos.</a>
+                @elseif ($events= 0 &&  count($events) == 0 )
+                    <p>Não há eventos disponíveis</p>
+                @endif
             </div>   
         </div>
 
